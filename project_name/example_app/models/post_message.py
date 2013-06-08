c37ac98bbf8bf07d67a0fa406583d6c8c72c86df
Jian-Long Huang <jianlong@ntu.edu.tw>
2013-06-08 21:39:02 +0800

    Use django timezone instead of datetime

--- project_name/example_app/models/post_message.py
+++ project_name/example_app/models/post_message.py
@@ -1,12 +1,12 @@
 from django.db import models
-import datetime
+from django.utils import timezone
 
 
 class PostMessage(models.Model):
     name = models.CharField(max_length=15)
     message = models.TextField()
     upload_file = models.TextField()
-    post_date = models.DateTimeField(default=datetime.datetime.now)
+    post_date = models.DateTimeField(default=timezone.now)
 
     class Meta:
         app_label = 'example_app'
